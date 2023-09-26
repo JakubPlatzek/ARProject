f9a900ceda04523ff181020c9d379ff8a71d86f0
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-09-26 10:22:48 +0200

    Fix UI flow

--- Assets/Scripts/SelectQuestHandler.cs
+++ Assets/Scripts/SelectQuestHandler.cs
@@ -9,6 +9,8 @@ public class SelectQuestHandler : MonoBehaviour
     public PlaceObject placeObject;
     public TMP_Dropdown dropdown;
     public GameObject questMenu;
+    public GameObject mainMenu;
+    public GameObject placementMenu;
     public List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
 
     void Awake(){
@@ -18,6 +20,8 @@ public class SelectQuestHandler : MonoBehaviour
 
     public void Evaluate(){
         placeObject.objectToPlace = dropdown.options[dropdown.value].text;
+        mainMenu.SetActive(false);
         questMenu.SetActive(false);
+        placementMenu.SetActive(true);
     }
 }
--- Assets/SelectPlacementModeHandler.cs
+++ Assets/SelectPlacementModeHandler.cs
@@ -7,12 +7,16 @@ public class SelectPlacementModeHandler : MonoBehaviour
 {
     public ARPointCloudManager ARPointCloudManager;
     public ARPlaneManager ARPlaneManager;
+    public GameObject ManualMode;
+    public GameObject PlacementModeMenu;
 
     public void SelectSpawnMode(){
         if(this.gameObject.name.Equals("Free hand mode")){
             ARPointCloudManager.enabled = false;
             ARPlaneManager.enabled = true;
         }else{
+            ManualMode.SetActive(true);
+            PlacementModeMenu.SetActive(false);
             ARPointCloudManager.enabled = true;
             ARPlaneManager.enabled = false;
         }
