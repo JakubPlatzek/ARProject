95487cb750f880f9bf4be545ddc90d8d1f870776
JakubPlatzek <72247219+JakubPlatzek@users.noreply.github.com>
2023-09-28 17:05:43 +0200

    More flexible manual mode

--- Assets/Scripts/ManualPlacement.cs
+++ Assets/Scripts/ManualPlacement.cs
@@ -6,6 +6,7 @@ using UnityEngine.Scripting;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using System.Linq;
 
 public class ManualPlacement : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class ManualPlacement : MonoBehaviour
     public Camera ARCamera;
     public SelectQuestHandler selectQuestHandler;
     List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+    List<GameObject> collectionPrefabs = new List<GameObject>();
     Vector3 evaluatedPosition = new Vector3();
     Vector3 evaluatedRotation = new Vector3();
 
@@ -27,9 +29,11 @@ public class ManualPlacement : MonoBehaviour
     void Update(){
         if(pressed && canPress){
             if(m_RaycastManager.Raycast(ARCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)), m_Hits, TrackableType.FeaturePoint)){
-                Instantiate(collectedPosition, m_Hits[0].pose.position, m_Hits[0].pose.rotation);
+                GameObject collectionPrefab = Instantiate(collectedPosition, m_Hits[0].pose.position, m_Hits[0].pose.rotation);
+                collectionPrefabs.Add(collectionPrefab);
                 tableCorners.Add(m_Hits[0].pose.position);
                 if(tableCorners.Count == 3){
+                    EvaluateMeasuredPositions();
                     EvaluateCollection();
                     PositionChosenQuestLayout();
                     GetComponent<Button>().interactable = false;
@@ -59,8 +63,31 @@ public class ManualPlacement : MonoBehaviour
         evaluatedPosition.y = averageHeight;
         evaluatedRotation = vectorAtoB;
     }
+    /**
+        Figure out biggest angle and set the belonging corner into the middle position of the list
+    */
+    void EvaluateMeasuredPositions(){
+        float angle01 = Vector3.Angle(tableCorners[0] - tableCorners[1], tableCorners[2] - tableCorners[1]);
+        float angle02 = Vector3.Angle(tableCorners[0] - tableCorners[2], tableCorners[1] - tableCorners[2]);
+        float angle12 = Vector3.Angle(tableCorners[1] - tableCorners[0], tableCorners[2] - tableCorners[0]);
+        float[] arrayOfAngles = {angle12, angle01, angle02};
+        int index = arrayOfAngles.ToList().IndexOf(arrayOfAngles.Max());
+        Vector3 tmp = tableCorners[1];
+        tableCorners[1] = tableCorners[index];
+        tableCorners[index] = tmp;
+    }
 
     void PositionChosenQuestLayout(){
-        Instantiate(Resources.Load("Quest 1"), evaluatedPosition, Quaternion.LookRotation(evaluatedRotation));
+        Instantiate(Resources.Load(selectQuestHandler.placeObject.objectToPlace), evaluatedPosition, Quaternion.LookRotation(evaluatedRotation));
+    }
+
+    public void Reset(){
+        tableCorners.Clear();
+        pressed = false;
+        canPress = true;
+        GetComponent<Button>().interactable = true;
+        foreach(GameObject collectionPrefab in collectionPrefabs){
+            Destroy(collectionPrefab);
+        }
     }
 }
