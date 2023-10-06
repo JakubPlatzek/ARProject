1acf8349a683a3c7cec505e7abd21ce68692616b
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-10-06 13:21:46 +0200

    Add script for character sheet to face camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    private void Start()
+    {
+        // Find the main camera in the scene
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        // Rotate the prefab to face the camera
+        transform.LookAt(transform.position + mainCameraTransform.rotation * Vector3.forward, mainCameraTransform.rotation * Vector3.up);
+    }
+}
