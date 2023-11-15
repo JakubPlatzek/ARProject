36c24315843e7297b42fe3f72076c4533f586480
Mansory888 <nichita.railean@aol.com>
2023-11-15 14:20:01 +0100

    bug fix

--- Assets/Scripts/ClearPrefabHandler.cs
+++ Assets/Scripts/ClearPrefabHandler.cs
@@ -7,5 +7,12 @@ public class ClearPrefabHandler : MonoBehaviour
     public void RemoveLayout(){
         Destroy(placeObject.placedLayout);
         gameObject.SetActive(false);
+        
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("popUP");
+        foreach (var obj in objectsWithTag)
+        {
+            Destroy(obj);
+        }
+        
     }
 }
